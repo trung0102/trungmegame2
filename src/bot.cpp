f22f3fa1465a7423aed3b73ce3ac3c4deeb8d9f5
trung0102 <trung.nguyen@hcmut.edu.vn>
2025-10-01 20:36:38 +0700

    update keyboard event & assets

---
+++ src/bot.cpp
@@ -0,0 +1,116 @@
+#include "include/bot.h"
+
+// Character::Character(/* args */)
+// {
+// }
+
+// Character::~Character()
+// {
+// }
+unordered_map<PlayerAction, ActionData> actionData = {
+    { PlayerAction::Idle,           {12, loadSurface("assets/playerIdle.png")} },
+    { PlayerAction::MoveForward,    {12, loadSurface("assets/playerRun.png")} },
+    { PlayerAction::MoveBackward,   {12, loadSurface("assets/playerRun.png")} },
+    { PlayerAction::Spike,          {13, loadSurface("assets/playerSmash.png")} },
+    { PlayerAction::Block,          {13, loadSurface("assets/playerBlock.png")} },
+    { PlayerAction::Dive,           {15, loadSurface("assets/playerSlide.png")} },
+    { PlayerAction::Pass,           {11, loadSurface("assets/playerReception.png")} },
+};
+
+Character:: Character(tuple<int, int> position, tuple<int, int> patrol_range){
+    ActionData action = actionData[this->status];
+    this->position = position;
+    this->patrol_range = patrol_range;
+    this->max_frame = action.maxframe;
+    this->surface = action.surface;
+    
+}
+Character:: ~Character(){
+    SDL_DestroySurface(this->surface);
+}
+void Character::update_position(){
+    this->current_frame = (this->current_frame +1) % this->max_frame;
+    get<0>(this->position) += 10;
+    this->srcRect.x = this->current_frame*32;
+    // this->dstRect.x += this->speed;
+}
+void Character::render(SDL_Surface* screenSurface){
+    SDL_BlitSurfaceScaled(this->surface, &this->srcRect, screenSurface, &this->dstRect, SDL_SCALEMODE_LINEAR);
+}
+void Character::getKeyboardEvent(SDL_KeyboardEvent keyEvent){
+    PlayerAction curr_status = this->status;
+    if (keyEvent.type == SDL_EVENT_KEY_UP){
+        this->status = PlayerAction::Idle;
+    }
+    else{
+        switch (keyEvent.scancode){
+        case SDL_SCANCODE_A:
+            this->status = PlayerAction::MoveBackward;
+            break;
+        case SDL_SCANCODE_D:
+            this->status = PlayerAction::MoveForward;
+            break;
+        case SDL_SCANCODE_S:
+            this->status = PlayerAction::Pass;
+            break;
+        case SDL_SCANCODE_W:
+            this->status = PlayerAction::Spike;
+            break;
+        case SDL_SCANCODE_UNKNOWN:
+            this->status = PlayerAction::Idle;
+            break;
+        }
+    }
+    
+    if(curr_status != this->status) this->current_frame = 0;
+    ActionData action = actionData[status];
+    this->max_frame = action.maxframe;
+    this->surface = action.surface;
+}
+
+Ball:: Ball(tuple<int, int> position, int max_frame){
+    this->current_frame = 0;
+    this->position = position;
+    this->max_frame = max_frame;
+    this->surface = loadSurface("assets/playerRun.png");
+    
+}
+Ball:: ~Ball(){
+    SDL_DestroySurface(this->surface);
+}
+void Ball::update_position(){
+    this->current_frame = (this->current_frame +1) % this->max_frame;
+    get<0>(this->position) += 10;
+    this->srcRect.x = this->current_frame*32;
+    // this->dstRect.x += this->speed;
+}
+void Ball::render(SDL_Surface* screenSurface){
+    SDL_BlitSurfaceScaled(this->surface, &this->srcRect, screenSurface, &this->dstRect, SDL_SCALEMODE_LINEAR);
+}
+
+SDL_Surface* loadSurface( std::string path)
+{
+	//The final optimized image
+	SDL_Surface* optimizedSurface = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+	if( loadedSurface == NULL )
+	{
+		SDL_Log( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), SDL_GetError() );
+	}
+	else
+	{
+		//Convert surface to screen format
+		optimizedSurface = SDL_ConvertSurface( loadedSurface, SDL_PIXELFORMAT_RGBA8888 );
+		if( optimizedSurface == NULL )
+		{
+			SDL_Log( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+		}
+
+		//Get rid of old loaded surface
+		SDL_DestroySurface( loadedSurface );
+	}
+
+	return optimizedSurface;
+}
---
+++ src/include/bot.h
@@ -0,0 +1,69 @@
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <stdio.h>
+#include <unordered_map>
+#include <tuple>
+#include <iostream>
+using namespace std;
+
+SDL_Surface* loadSurface( std::string path);
+
+enum class PlayerAction {
+    Idle,                           // dung yen
+    MoveForward,                    // tien
+    MoveBackward,                   // lui
+    Spike,                          // dap bong
+    Block,                          // bật nhảy chắn bóng đối phương.
+    Dive,                           // lao người cứu bóng
+    Pass                            // chuyền bóng thấp
+};
+
+struct ActionData{
+    int maxframe;
+    SDL_Surface* surface;
+}; 
+// maxframe, url_asset
+
+// Map từ enum -> dữ liệu
+extern unordered_map<PlayerAction, ActionData> actionData;
+
+class Character
+{
+private:
+    /* data */
+    PlayerAction status = PlayerAction::Idle;
+    tuple<int, int> position;
+    tuple<int, int> patrol_range;
+    int current_frame = 0;
+    int max_frame;
+    int speed = 0.5;
+    SDL_Surface* surface;
+    SDL_Rect srcRect = {0, 0, 32, 48};
+    SDL_Rect dstRect = {500, 500, 32*2, 48*2};
+
+public:
+    Character(tuple<int, int> position, tuple<int, int> patrol_range);
+    ~Character();
+    void render(SDL_Surface* screenSurface);
+    void update_position();
+    void getKeyboardEvent(SDL_KeyboardEvent keyEvent);
+};
+
+class Ball
+{
+private:
+    /* data */
+    int current_frame;
+    int max_frame;
+    int speed = 0.5;
+    tuple<int, int> position;
+    SDL_Surface* surface;
+    SDL_Rect srcRect = {0, 0, 32, 48};
+    SDL_Rect dstRect = {500, 500, 32*2, 48*2};
+    
+public:
+    Ball(tuple<int, int> position, int max_frame);
+    ~Ball();
+    void update_position();
+    void render(SDL_Surface* screenSurface);
+};
